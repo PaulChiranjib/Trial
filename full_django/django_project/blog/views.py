760f357a8aded03337f6df0916e9f8e2805e655b
PaulChiranjib <chiranjibpaul64@gmail.com>
2023-04-30 14:37:44 +0530

    initial commit
    password reset option is not yet available

---
+++ full_django/django_project/blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
+from .models import post
+
+
+def home(request):
+    context={
+        'posts':post.objects.all()
+    }
+    return render(request, "blog/home.html",context)
+
+class PostListView(ListView):
+    model=post
+    template_name='blog/home.html'  #<app>/<model>_<classtype>.html
+    context_object_name='posts'
+    ordering=['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model=post
+
+def about(request):
+    return render(request, "blog/about.html",{'title':'about'})
