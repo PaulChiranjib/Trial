2df93fd3a95e13496495105ec7a3e5c26c150dca
PaulChiranjib <chiranjibpaul64@gmail.com>
2023-05-05 13:39:48 +0530

    added new feature CRUD
    Added feature as Create Post, Update Post and Delete Post

--- full_django/django_project/blog/views.py
+++ full_django/django_project/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import post
-
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 def home(request):
     context={
@@ -19,6 +19,37 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model=post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=post
+    fields=['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=post
+    fields=['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=post
+    success_url='/'
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, "blog/about.html",{'title':'about'})
